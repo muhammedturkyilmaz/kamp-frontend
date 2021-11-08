3cfb97b816dfc1b31c8ce0c5ef84b74f538ba6ce
muhammedturkyilmaz <muhammed.turkyilmaz29@gmail.com>
2021-11-08 19:24:39 +0300

    8 Kasım Commit 2

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -16,7 +17,8 @@ export class ProductComponent implements OnInit {
   filterText="";
 
   constructor(private productService: ProductService, private activetedRoute:ActivatedRoute,
-    private toastrService:ToastrService) { }
+    private toastrService:ToastrService,
+    private cartService:CartService) { }
 
   ngOnInit(): void {
 
@@ -47,6 +49,7 @@ export class ProductComponent implements OnInit {
 
   addToCart(product:Product){
     this.toastrService.success("Sepete eklendi",product.productName)
+    this.cartService.addToCart(product);
   }
 
 }
